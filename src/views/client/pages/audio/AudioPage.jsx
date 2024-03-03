8ad479a4c84c60ce3ba454035be05b1a36a715ec
AstralisMC <153031380+astralishost@users.noreply.github.com>
2024-03-03 16:12:41 +0100

    unzipgit

---
+++ src/views/client/pages/audio/AudioPage.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Header } from '../../../../components/header/Header';
+import ClickPrompt from '../../../../components/clicktoconnect/ClickPrompt';
+import { VcOnboarding } from '../../../../components/onboarding/VcOnboarding';
+import AudioVolume from '../../../../components/audio/AudioVolume';
+import SoundCloudPlayer from '../../../../components/soundcloud/SoundCloudPlayer';
+import { UnsupportedBanner } from '../../../../components/voice/UnsupportedBanner';
+import { msg } from '../../../../client/OpenAudioAppContainer';
+
+function AudioPage(props) {
+  if (props.clickLock) {
+    return <ClickPrompt />;
+  }
+
+  return (
+    <>
+      <div className="h-full w-full audio-page-bg flex justify-center items-center">
+        <div className="xl:w-1/2">
+          <AudioVolume />
+          <Header />
+        </div>
+        <div className="xl:w-1/2">
+          {props.voiceState.enabled && !props.voiceState.ready && props.clientSupportsVoiceChat && props.browserSupportsVoiceChat
+            ? <VcOnboarding /> : null}
+          {props.voiceState.enabled && !props.browserSupportsVoiceChat
+            ? <UnsupportedBanner>{msg('vc.notCompatible')}</UnsupportedBanner> : null}
+          {props.voiceState.enabled && !props.clientSupportsVoiceChat
+            ? <UnsupportedBanner>{msg('vc.badClient')}</UnsupportedBanner> : null}
+        </div>
+
+      </div>
+      <SoundCloudPlayer />
+    </>
+  );
+}
+
+export default connect(mapStateToProps)(AudioPage);
+
+function mapStateToProps(state) {
+  return {
+    browserSupportsVoiceChat: state.browserSupportsVoiceChat,
+    voiceState: state.voiceState,
+    clientSupportsVoiceChat: state.clientSupportsVoiceChat,
+    clickLock: state.clickLock,
+  };
+}
